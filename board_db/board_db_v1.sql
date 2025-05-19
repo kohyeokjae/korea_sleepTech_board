1dc08485506ca687b1c07a530f7b1b37c96192ea
kohyeokjae <kohj0686@naver.com>
2025-05-19 10:49:04 +0900

    환경설정

---
+++ board_db/board_db_v1.sql
@@ -0,0 +1,18 @@
+# 한글 인코딩을 위한 UTF-8 설정
+CREATE DATABASE IF NOT EXISTS `board_db`
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+USE `board_db`;
+
+-- CREATE TABLE `users` (
+
+-- ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CREATE TABLE `articles` (
+
+-- ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CREATE TABLE `comments` (
+
+-- ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
